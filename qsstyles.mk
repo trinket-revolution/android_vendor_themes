ec1415c6bcb70e1f3043e731cf8ff699f9ebd84e
703joko <703joko@gmail.com>
2020-12-07 04:53:02 +0700

    Add brightness, qsshape, panelbg style [3/3]

--- qsstyles.mk
+++ qsstyles.mk
@@ -10,3 +10,38 @@ PRODUCT_PACKAGES += \
     QStileNinja \
     QStilePokesign \
     QStileWavey
+
+# Panel Background
+PRODUCT_PACKAGES += \
+    PanelBgBatik \
+    PanelBgKece \
+    PanelBgOutline
+
+# Qs shape
+PRODUCT_PACKAGES += \
+    QsShapeA \
+    QsShapeB \
+    QsShapeC \
+    QsShapeD \
+    QsShapeE \
+    QsShapeF \
+    QsShapeG \
+    QsShapeH \
+    QsShapeI \
+    QsShapeJ \
+    QsShapeK \
+    QsShapeL \
+    QsShapeM \
+    QsShapeN \
+    QsShapeO \
+    QsShapeP \
+    QsShapeQ \
+    QsShapeR \
+    QsShapeS 
+
+# Brightness sliders Styles
+PRODUCT_PACKAGES += \
+    BrightnessTypeA \
+    BrightnessTypeB \
+    BrightnessTypeC \
+    BrightnessTypeD  
